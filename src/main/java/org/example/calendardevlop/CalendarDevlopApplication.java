b33597498a665c7d597bcd82f79fae2d72e398c6
kimseokjun <k03s13j@naver.com>
2025-08-08 18:56:04 +0900

    first commit

---
+++ src/main/java/org/example/calendardevlop/CalendarDevlopApplication.java
@@ -0,0 +1,13 @@
+package org.example.calendardevlop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarDevlopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarDevlopApplication.class, args);
+    }
+
+}
